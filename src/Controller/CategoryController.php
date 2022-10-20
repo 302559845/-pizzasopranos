2b58daf0576c7e94c94a9f307dba13378d604ab8
Emmanuel Owusu <70710188+302559845@users.noreply.github.com>
2022-10-20 12:38:54 +0200

    nieuwe pagina gemaakt

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    #[Route('/category/{id}', name: 'app_category')]
+    public function index($id, ProductsRepository $productsRepository): Response
     {
+//        $products = $productsRepository->findBy(array('category' => $id));
+        $products = $productsRepository->findAll();
+
+//        dd($products);
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'products' => $products,
         ]);
     }
 }
--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -22,6 +22,9 @@ class Products
     #[ORM\Column]
     private ?float $price = null;
 
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Category $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +62,18 @@ class Products
     public function setPrice(float $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
