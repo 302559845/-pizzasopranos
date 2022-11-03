f213d7e011069ffa7123138a371be31fe3084d97
Emmanuel Owusu <302559845@student.rocmondriaan.nl>
2022-11-03 09:54:35 +0100

    nieuwe databse toegevoegd

--- products (1).sql
+++ products (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 20 okt 2022 om 12:40
--- Serverversie: 10.4.24-MariaDB
--- PHP-versie: 8.1.6
+-- Gegenereerd op: 03 nov 2022 om 09:53
+-- Serverversie: 10.4.25-MariaDB
+-- PHP-versie: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,18 +31,16 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` double NOT NULL,
-  `category_id` int(11) DEFAULT NULL
+  `price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `image`, `price`, `category_id`) VALUES
-(1, 'BBQ MEATLOVERS PIZZA', '/img/bbqpizza.jpg', 16, 1),
-(2, 'Double Pepperoni pizza', '/img/pepperonipizza.jpg', 16.5, 1),
-(3, 'Salami pizza', '/img/salami-7743.jpg', 16.55, 1);
+INSERT INTO `products` (`id`, `name`, `image`, `price`) VALUES
+(1, 'BBQ MEATLOVERS PIZZA', '/img/bbqpizza.jpg', 16.5),
+(2, 'Double Pepperoni pizza', '/img/pepperonipizza.jpg', 16.5);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -52,8 +50,7 @@ INSERT INTO `products` (`id`, `name`, `image`, `price`, `category_id`) VALUES
 -- Indexen voor tabel `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_B3BA5A5A12469DE2` (`category_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -63,7 +60,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT voor een tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
@@ -73,7 +70,7 @@ ALTER TABLE `products`
 -- Beperkingen voor tabel `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `FK_B3BA5A5A12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
