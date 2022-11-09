09b5c9f692f05f53493d1ba861eeae4a47c6be35
302559845 <302559845@student.rocmondriaan.nl>
2022-11-09 12:39:45 +0100

    kan een product tonen

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product/{id}', name: 'app_product')]
+    public function index(ProductsRepository $productsRepository, int $id): Response
     {
+        $products = $productsRepository->find( $id);
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 }
